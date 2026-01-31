8c933ddc79f88015bcfe779b534fd3da9b44d165
JavaKoala <javakoala1@gmail.com>
2026-01-31 17:56:18 -0500

    Display note

--- spec/requests/notes_spec.rb
+++ spec/requests/notes_spec.rb
@@ -1,7 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'Notes', type: :request do
+  before do
+    Note.find_or_create_by(id: 1)
+  end
+
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'returns http success' do
+      get notes_path
+
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'PATCH /notes/:id' do
+    it 'updates the note' do
+      note = Note.find(1)
+      patch note_path(note),
+            params: { note: { content: 'test' } }
+
+      expect(note.reload.content.body.to_plain_text).to eq('test')
+    end
   end
 end

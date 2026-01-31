5c5daf0314641f04d54a393de94b55dc0d982392
JavaKoala <javakoala1@gmail.com>
2026-01-31 18:27:17 -0500

    Do not need action text

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,4 +7,3 @@ pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin 'lexxy', to: 'lexxy.js'
 pin '@rails/activestorage', to: 'activestorage.esm.js'
-pin '@rails/actiontext', to: '@rails--actiontext.js' # @8.1.200

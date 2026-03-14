7709ef7722aef297d972278765cc2ff3cac5ed32
JavaKoala <javakoala1@gmail.com>
2026-03-14 15:31:48 -0400

    Change refresh target

--- app/javascript/controllers/loading_controller.js
+++ app/javascript/controllers/loading_controller.js
@@ -1,9 +1,9 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "refresh" ]
+  static targets = [ "note" ]
 
   refresh() {
-    this.refreshTarget.classList.add("opacity-50")
+    this.noteTarget.classList.add("opacity-50")
   }
 }

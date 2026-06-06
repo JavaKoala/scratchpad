e4879322119f2e7c699e443e1081ccbc16bac7e6
JavaKoala <javakoala1@gmail.com>
2026-06-06 11:03:35 -0400

    Fix rubocop warnings

--- spec/system/notes_spec.rb
+++ spec/system/notes_spec.rb
@@ -8,26 +8,26 @@ RSpec.describe 'Notes', type: :system do
   it 'creates a note' do
     visit '/notes'
 
-    expect(page).to have_content('Scratchpad')
+    expect(page).to have_text('Scratchpad')
 
     find_by_id('note_content-content').click.set('I can haz note')
     click_button('Save')
 
-    expect(page).to have_content('I can haz note')
+    expect(page).to have_text('I can haz note')
     expect(Note.find(1).content.to_plain_text).to eq('I can haz note')
   end
 
   it 'refreshes content' do
     visit '/notes'
 
-    expect(page).to have_content('Scratchpad')
-    expect(page).to have_no_content('New note')
+    expect(page).to have_text('Scratchpad')
+    expect(page).to have_no_text('New note')
 
     Note.find(1).update!(content: 'New note')
 
     click_link('Refresh')
 
-    expect(page).to have_content('New note')
+    expect(page).to have_text('New note')
   end
 
   it 'clears content' do
@@ -36,11 +36,11 @@ RSpec.describe 'Notes', type: :system do
 
     visit '/notes'
 
-    expect(page).to have_content('Old note')
+    expect(page).to have_text('Old note')
 
     click_button('Clear')
 
-    expect(page).to have_no_content('Old note')
+    expect(page).to have_no_text('Old note')
     expect(note.reload.content.to_plain_text).to be_empty
   end
 end
